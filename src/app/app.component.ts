269a3e2f59559c1a79a20ff229eee11b74246349
idan.h <idan0540@gmail.com>
2020-05-05 16:28:44 +0300

    fixed nav, made dynamic notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  notes = [
+    {
+      title: 'my note',
+      desc: '.....'
+    },
+    {
+      title: 'my note 2',
+      desc: '.....'
+    },
+    {
+      title: 'my note 3',
+      desc: '.....'
+    },
+    {
+      title: 'my note 4',
+      desc: '.....'
+    }
+  ];
+
+
+  newNote() {
+    console.log("hiiiii");
+    
+  }
 }
